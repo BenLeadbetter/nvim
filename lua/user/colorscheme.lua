7bd51435ae01c6e0df7620f2ed3c21bc8132e9e6
Ben Leadbetter - Mac2 <ben.leadbetter@native-instruments.com>
2023-04-08 21:19:06 +0200

    fixes first bracket red with rainbow-ts

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,12 @@
 local colorscheme = "nightfly"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+
+if colorscheme == "nightfly" then
+    -- first bracket appears pink rather than red
+    vim.cmd(":hi rainbowcol1 guifg=#FF06BC")
+end
+
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found!")
 	return
