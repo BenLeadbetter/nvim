74b38c20c46b36065f875edc625529ae1358ff72
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2024-06-15 10:39:14 +0200

    refactor: reformat keymaps file

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -37,21 +37,21 @@ keymap("n", "<leader>rg", "<cmd>Telescope live_grep<cr>", opts)
 
 -- formatting
 local range_format = function()
-	local start_row, _ = vim.api.nvim_buf_get_mark(0, "<")
-	local end_row, _ = vim.api.nvim_buf_get_mark(0, ">")
-	vim.lsp.buf.format({
-		range = {
-			["start"] = { start_row, 0 },
-			["end"] = { end_row, 0 },
-		},
-		async = true,
-		normal = true,
-	})
+    local start_row, _ = vim.api.nvim_buf_get_mark(0, "<")
+    local end_row, _ = vim.api.nvim_buf_get_mark(0, ">")
+    vim.lsp.buf.format({
+        range = {
+            ["start"] = { start_row, 0 },
+            ["end"] = { end_row, 0 },
+        },
+        async = true,
+        normal = true,
+    })
 end
 vim.keymap.set("v", "<leader>F", range_format)
 keymap("n", "<leader>F", ":lua vim.lsp.buf.format()<cr>", opts)
 
-local function with_module(module_name ,f)
+local function with_module(module_name, f)
     return function()
         local has_module, module = pcall(require, module_name)
         if has_module then
@@ -68,110 +68,80 @@ local function with_tc(f)
     return with_module("textcase", f)
 end
 
-local function with_persisten_breakpoints(f)
+local function change_current_word(to_case)
+    return function(tc)
+        tc.current_word(to_case)
+    end
+end
+
+local function with_persistent_breakpoints(f)
     return with_module("persistent-breakpoints.api", f)
 end
 
 -- text-case
-vim.keymap.set('n', '<leader>tcu', with_tc(function(tc) tc.current_word('to_upper_case') end))
-vim.keymap.set('n', '<leader>tcl', with_tc(function(tc) tc.current_word('to_lower_case') end))
-vim.keymap.set('n', '<leader>tcs', with_tc(function(tc) tc.current_word('to_snake_case') end))
-vim.keymap.set('n', '<leader>tcd', with_tc(function(tc) tc.current_word('to_dash_case') end))
-vim.keymap.set('n', '<leader>tcn', with_tc(function(tc) tc.current_word('to_constant_case') end))
-vim.keymap.set('n', '<leader>tcd', with_tc(function(tc) tc.current_word('to_dot_case') end))
-vim.keymap.set('n', '<leader>tca', with_tc(function(tc) tc.current_word('to_phrase_case') end))
-vim.keymap.set('n', '<leader>tcc', with_tc(function(tc) tc.current_word('to_camel_case') end))
-vim.keymap.set('n', '<leader>tcp', with_tc(function(tc) tc.current_word('to_pascal_case') end))
-vim.keymap.set('n', '<leader>tct', with_tc(function(tc) tc.current_word('to_title_case') end))
-vim.keymap.set('n', '<leader>tcf', with_tc(function(tc) tc.current_word('to_path_case') end))
+vim.keymap.set("n", "<leader>tcu", with_tc(change_current_word("to_upper_case")))
+vim.keymap.set("n", "<leader>tcl", with_tc(change_current_word("to_lower_case")))
+vim.keymap.set("n", "<leader>tcs", with_tc(change_current_word("to_snake_case")))
+vim.keymap.set("n", "<leader>tcd", with_tc(change_current_word("to_dash_case")))
+vim.keymap.set("n", "<leader>tcn", with_tc(change_current_word("to_constant_case")))
+vim.keymap.set("n", "<leader>tcd", with_tc(change_current_word("to_dot_case")))
+vim.keymap.set("n", "<leader>tca", with_tc(change_current_word("to_phrase_case")))
+vim.keymap.set("n", "<leader>tcc", with_tc(change_current_word("to_camel_case")))
+vim.keymap.set("n", "<leader>tcp", with_tc(change_current_word("to_pascal_case")))
+vim.keymap.set("n", "<leader>tct", with_tc(change_current_word("to_title_case")))
+vim.keymap.set("n", "<leader>tcf", with_tc(change_current_word("to_path_case")))
+
+local dap_commands = {
+    continue = with_dap(function(dap)
+        dap.continue()
+    end),
+    step_over = with_dap(function(dap)
+        dap.step_over()
+    end),
+    step_into = with_dap(function(dap)
+        dap.step_into()
+    end),
+    step_out = with_dap(function(dap)
+        dap.step_out()
+    end),
+    toggle_breakpoint = with_persistent_breakpoints(function(dap)
+        dap.step_out()
+    end),
+    set_log_point = with_dap(function(dap)
+        dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+    end),
+    open_repl = with_dap(function(dap)
+        dap.repl.open()
+    end),
+    debug_hover = with_dap(function(dap)
+        dap.ui.widgets.hover()
+    end),
+    debug_preview = with_dap(function(dap)
+        dap.ui.widgets.preview()
+    end),
+}
 
 -- debugging
-vim.keymap.set(
-	"n",
-	"<F5>",
-	with_dap(function(dap)
-		dap.continue()
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<leader>do",
-	with_dap(function(dap)
-		dap.step_over()
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<leader>di",
-	with_dap(function(dap)
-		dap.step_into()
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<leader>dO",
-	with_dap(function(dap)
-		dap.step_out()
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<Leader>db",
-	with_persisten_breakpoints(function(mod)
-		mod.toggle_breakpoint()
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<Leader>dB",
-	with_dap(function(dap)
-		dap.set_breakpoint()
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<Leader>dlp",
-	with_dap(function(dap)
-		dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<Leader>dr",
-	with_dap(function(dap)
-		dap.repl.open()
-	end)
-)
-vim.keymap.set(
-	"n",
-	"<Leader>dl",
-	with_dap(function(dap)
-		dap.run_last()
-	end)
-)
-vim.keymap.set(
-	{ "n", "v" },
-	"<Leader>dh",
-	with_dap(function(dap)
-		dap.ui.widgets.hover()
-	end)
-)
-vim.keymap.set(
-	{ "n", "v" },
-	"<Leader>dp",
-	with_dap(function(dap)
-		dap.ui.widgets.preview()
-	end)
-)
+vim.keymap.set("n", "<F5>", dap_commands.continue)
+vim.keymap.set("n", "<leader>do", dap_commands.step_over)
+vim.keymap.set("n", "<leader>di", dap_commands.step_into)
+vim.keymap.set("n", "<leader>dO", dap_commands.step_out)
+vim.keymap.set("n", "<Leader>db", dap_commands.toggle_breakpoint)
+vim.keymap.set("n", "<Leader>dlp", dap_commands.set_log_point)
+vim.keymap.set("n", "<Leader>dr", dap_commands.open_repl)
+vim.keymap.set({ "n", "v" }, "<Leader>dh", dap_commands.debug_hover)
+vim.keymap.set({ "n", "v" }, "<Leader>dp", dap_commands.debug_preview)
 vim.keymap.set("n", "<Leader>dg", function()
-	local has_dapui, dapui = pcall(require, "dapui")
-	if has_dapui then
-		dapui.toggle()
-	end
+    local has_dapui, dapui = pcall(require, "dapui")
+    if has_dapui then
+        dapui.toggle()
+    end
 end)
 
 -- ai
 if pcall(require, "chatgpt") then
-    vim.keymap.set("v", "<leader>ai", function() require("chatgpt").edit_with_instructions() end)
+    vim.keymap.set("v", "<leader>ai", function()
+        require("chatgpt").edit_with_instructions()
+    end)
     keymap("n", "<leader>ai", ":ChatGPT<cr>", opts)
 end
