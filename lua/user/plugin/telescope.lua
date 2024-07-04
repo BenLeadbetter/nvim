6372471107d1a75510fafd03a474fa2b543a2723
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2024-07-04 14:52:19 +0200

    refactor: telescope cleanup

--- lua/user/plugin/telescope.lua
+++ lua/user/plugin/telescope.lua
@@ -1,9 +1,9 @@
 return {
-	"nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope.nvim",
     version = "*",
     keys = {
-        { "<leader>f", "<cmd>Telescope find_files no_ignore=true theme=dropdown<cr>", desc = "File files" },
-        { "<leader>rg", "<cmd>Telescope live_grep<cr>", desc = "Grep all files" },
+        { "<leader>f",  "<cmd>Telescope find_files no_ignore=true theme=dropdown<cr>", desc = "File files" },
+        { "<leader>rg", "<cmd>Telescope live_grep<cr>",                                desc = "Grep all files" },
     },
     config = function()
         local actions = require("telescope.actions")
@@ -13,80 +13,80 @@ return {
                 prompt_prefix = " ",
                 selection_caret = " ",
                 path_display = { "truncate" },
-               mappings = {
-                   i = {
-                    ["<C-n>"] = actions.cycle_history_next,
-                    ["<C-p>"] = actions.cycle_history_prev,
+                mappings = {
+                    i = {
+                        ["<C-n>"] = actions.cycle_history_next,
+                        ["<C-p>"] = actions.cycle_history_prev,
 
-                    ["<C-j>"] = actions.move_selection_next,
-                    ["<C-k>"] = actions.move_selection_previous,
+                        ["<C-j>"] = actions.move_selection_next,
+                        ["<C-k>"] = actions.move_selection_previous,
 
-                    ["<C-c>"] = actions.close,
-                    ["<leader>e"] = actions.close,
- 
-                    ["<Down>"] = actions.move_selection_next,
-                    ["<Up>"] = actions.move_selection_previous,
- 
-                    ["<CR>"] = actions.select_default,
-                    ["<C-x>"] = actions.select_horizontal,
-                    ["<C-v>"] = actions.select_vertical,
-                    ["<C-t>"] = actions.select_tab,
- 
-                    ["<C-u>"] = actions.preview_scrolling_up,
-                    ["<C-d>"] = actions.preview_scrolling_down,
- 
-                    ["<PageUp>"] = actions.results_scrolling_up,
-                    ["<PageDown>"] = actions.results_scrolling_down,
- 
-                    ["<C-l>"] = actions.complete_tag,
-                    ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-                      },
- 
-                n = {
-                    ["<leader>e"] = actions.close,
-                    ["<esc>"] = actions.close,
-                    ["<CR>"] = actions.select_default,
-                    ["<C-x>"] = actions.select_horizontal,
-                    ["<C-v>"] = actions.select_vertical,
-                    ["<C-t>"] = actions.select_tab,
- 
-                    ["j"] = actions.move_selection_next,
-                    ["k"] = actions.move_selection_previous,
-                    ["H"] = actions.move_to_top,
-                    ["M"] = actions.move_to_middle,
-                    ["L"] = actions.move_to_bottom,
- 
-                    ["<Down>"] = actions.move_selection_next,
-                    ["<Up>"] = actions.move_selection_previous,
-                    ["gg"] = actions.move_to_top,
-                    ["G"] = actions.move_to_bottom,
- 
-                    ["<C-u>"] = actions.preview_scrolling_up,
-                    ["<C-d>"] = actions.preview_scrolling_down,
- 
-                    ["<PageUp>"] = actions.results_scrolling_up,
-                    ["<PageDown>"] = actions.results_scrolling_down,
- 
-                    ["?"] = actions.which_key,
+                        ["<C-c>"] = actions.close,
+                        ["<leader>e"] = actions.close,
+
+                        ["<Down>"] = actions.move_selection_next,
+                        ["<Up>"] = actions.move_selection_previous,
+
+                        ["<CR>"] = actions.select_default,
+                        ["<C-x>"] = actions.select_horizontal,
+                        ["<C-v>"] = actions.select_vertical,
+                        ["<C-t>"] = actions.select_tab,
+
+                        ["<C-u>"] = actions.preview_scrolling_up,
+                        ["<C-d>"] = actions.preview_scrolling_down,
+
+                        ["<PageUp>"] = actions.results_scrolling_up,
+                        ["<PageDown>"] = actions.results_scrolling_down,
+
+                        ["<C-l>"] = actions.complete_tag,
+                        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+                    },
+
+                    n = {
+                        ["<leader>e"] = actions.close,
+                        ["<esc>"] = actions.close,
+                        ["<CR>"] = actions.select_default,
+                        ["<C-x>"] = actions.select_horizontal,
+                        ["<C-v>"] = actions.select_vertical,
+                        ["<C-t>"] = actions.select_tab,
+
+                        ["j"] = actions.move_selection_next,
+                        ["k"] = actions.move_selection_previous,
+                        ["H"] = actions.move_to_top,
+                        ["M"] = actions.move_to_middle,
+                        ["L"] = actions.move_to_bottom,
+
+                        ["<Down>"] = actions.move_selection_next,
+                        ["<Up>"] = actions.move_selection_previous,
+                        ["gg"] = actions.move_to_top,
+                        ["G"] = actions.move_to_bottom,
+
+                        ["<C-u>"] = actions.preview_scrolling_up,
+                        ["<C-d>"] = actions.preview_scrolling_down,
+
+                        ["<PageUp>"] = actions.results_scrolling_up,
+                        ["<PageDown>"] = actions.results_scrolling_down,
+
+                        ["?"] = actions.which_key,
+                    },
                 },
-               },
             },
-        -- pickers = {
-        -- Default configuration for builtin pickers goes here:
-        -- picker_name = {
-        --   picker_config_key = value,
-        --   ...
-        -- }
-        -- Now the picker_config_key will be applied every time you call this
-        -- builtin picker
-        -- },
-        extensions = {
-            -- Your extension configuration goes here:
-            -- extension_name = {
-            --   extension_config_key = value,
+            -- pickers = {
+            -- Default configuration for builtin pickers goes here:
+            -- picker_name = {
+            --   picker_config_key = value,
+            --   ...
             -- }
-            -- please take a look at the readme of the extension you want to configure
-        },
+            -- Now the picker_config_key will be applied every time you call this
+            -- builtin picker
+            -- },
+            extensions = {
+                -- Your extension configuration goes here:
+                -- extension_name = {
+                --   extension_config_key = value,
+                -- }
+                -- please take a look at the readme of the extension you want to configure
+            },
         })
     end,
 }
