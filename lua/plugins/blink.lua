5471b85ca221037a39cff49cb0a36f713c318ca2
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2025-02-24 10:00:46 +0100

    feat: configure blink to previous custom cmp keymaps

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+	"saghen/blink.cmp",
+	opts = {
+        keymap = {
+            ['<C-k>'] = { 'select_prev', 'fallback', },
+            ['<C-j>'] = { 'select_next', 'fallback', },
+            ['<C-u>'] = { 'scroll_documentation_up', 'fallback', },
+            ['<C-d>'] = { 'scroll_documentation_down', 'fallback', },
+        },
+	},
+}
+
