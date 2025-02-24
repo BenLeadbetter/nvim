fd2314b332acea8abe980278acba20cd59acfbf2
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2025-02-24 10:13:44 +0100

    feat: blink-cmp completions are written to the . buffer

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,12 +1,16 @@
 return {
 	"saghen/blink.cmp",
 	opts = {
-        keymap = {
-            ['<C-k>'] = { 'select_prev', 'fallback', },
-            ['<C-j>'] = { 'select_next', 'fallback', },
-            ['<C-u>'] = { 'scroll_documentation_up', 'fallback', },
-            ['<C-d>'] = { 'scroll_documentation_down', 'fallback', },
-        },
+		completion = {
+			accept = {
+				dot_repeat = true,
+			},
+		},
+		keymap = {
+			["<C-k>"] = { "select_prev", "fallback" },
+			["<C-j>"] = { "select_next", "fallback" },
+			["<C-u>"] = { "scroll_documentation_up", "fallback" },
+			["<C-d>"] = { "scroll_documentation_down", "fallback" },
+		},
 	},
 }
-
