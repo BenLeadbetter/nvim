433106b80519b1e94f3bbbf22fa3fb8501a88064
Ben Leadbetter <ben.leadbetter@native-instruments.com>
2025-05-12 12:42:06 +0200

    fix: fix false-positive quote pair in rust buffers

---
+++ lua/plugins/mini-pairs.lua
@@ -0,0 +1,16 @@
+return {
+	"echasnovski/mini.pairs",
+	opts = function(_, opts)
+		local midi_pairs = require("mini.pairs")
+
+		-- Remove default rule for single quote in Rust
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "rust",
+			callback = function()
+				midi_pairs.unmap("i", "'", "'")
+			end,
+		})
+
+		return opts
+	end,
+}
